da27f4f12724a207801f1515462a95167ca6fc3c
TxS-7 <txs-1991@hotmail.com>
2020-07-07 00:58:43 +0300

    Add elf_utils function to check if a file is an ELF using the ELF magic number

--- elf_utils.c
+++ elf_utils.c
@@ -1,2 +1,13 @@
 #include "elf_utils.h"
-#include <elf.h>
+#include <string.h>
+
+int is_elf(const uint8_t *file_content, size_t file_size)
+{
+	const uint8_t elf_magic_number[] = { 0x7f, 'E', 'L', 'F' };
+
+	if (!file_content || file_size < sizeof(elf_magic_number)) {
+		return 0;
+	}
+
+	return memcmp(file_content, elf_magic_number, sizeof(elf_magic_number)) == 0;
+}
--- elf_utils.h
+++ elf_utils.h
@@ -1,6 +1,9 @@
 #ifndef ELF_UTILS_H
 #define ELF_UTILS_H
 
+#include <stdint.h>
+#include <stddef.h>
 
+int is_elf(const uint8_t *file_content, size_t file_size);
 
 #endif // ELF_UTILS_H
