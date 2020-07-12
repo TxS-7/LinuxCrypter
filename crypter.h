890623074c93f8a9322f65774e89b01667dea825
TxS-7 <txs-1991@hotmail.com>
2020-07-12 14:35:11 +0300

    Take output filename as argument instead of #define, copy stub to output filename to make it reusable, improve error handling and logging

--- crypter.h
+++ crypter.h
@@ -6,10 +6,9 @@
 #define SUCCESS 0
 #define FAIL -1
 #define CRYPTER_MAX_PAYLOAD_SIZE 100000 // 100 KB
-#define CRYPTER_OUTPUT_FILENAME "output"
 
 extern const uint8_t g_encryption_key[8];
 
-int encrypt_file(const char *payload_filename, const char *stub_filename);
+int encrypt_file(const char *payload_filename, const char *stub_filename, const char *output_filename);
 
 #endif // CRYPTER_H
--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3) {
-		printf("Usage: %s <file to encrypt> <stub file>\n", argv[0]);
+	if (argc != 4) {
+		printf("Usage: %s <file to encrypt> <stub file> <output file>\n", argv[0]);
 		return -1;
 	}
 
-	encrypt_file(argv[1], argv[2]);
+	if (encrypt_file(argv[1], argv[2], argv[3]) == FAIL) {
+		printf("Crypter failed\n");
+		return -1;
+	}
+
+	printf("Generated file: %s\n", argv[3]);
 	return 0;
 }
